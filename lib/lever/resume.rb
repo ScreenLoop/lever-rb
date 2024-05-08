c582b8ebce925ddf514186bf9b66ecdf54d01e6b
Rodrigo Oliveira Santos <rodriisantos@gmail.com>
2024-05-08 17:09:07 +0100

    Add support to read application resumes (#6)
    SL-4382

---
+++ lib/lever/resume.rb
@@ -0,0 +1,10 @@
+require 'lever/base'
+
+module Lever
+  class Resume < Base
+    property :id
+    property :created_at, from: :createdAt
+    property :file
+    property :parsed_data, from: :parsedData
+  end
+end
