e5d91af8ef0c09b530aa594a36cd27a5faeb40d1
Rodrigo Santos <rodriisantos@gmail.com>
2024-05-08 14:59:54 +0100

    try 2344

--- lib/lever/resume.rb
+++ lib/lever/resume.rb
@@ -4,14 +4,7 @@ module Lever
   class Resume < Base
     property :id
     property :created_at, from: :createdAt
-    property :file, from: :parse_resume_file
+    property :file
     property :parsed_data, from: :parsedData
-
-    def parse_resume_file
-      raise Error, "what the file? #{file&.to_json}"
-      return if file.blank?
-
-      Lever::File.new(file.merge(client: client))
-    end
   end
 end
